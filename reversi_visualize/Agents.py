9c928114d3fc67e6e903a25e1745e280b7dc1964
Đặng Trọng Nhân <dangtrongnhan@MacBook-Air-cua-ang.local>
2023-04-11 14:01:02 +0700

    Add minimax and alpha beta

--- reversi_visualize/Agents.py
+++ reversi_visualize/Agents.py
@@ -1,5 +1,14 @@
 
 import random as rd
+import numpy as np
+import sys
+import copy
+
+WHITE = 1
+NOBODY = 0
+BLACK = -1
+
+NUM_COLUMNS = 8
 
 board = [[0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,7 +19,9 @@ board = [[0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],]
 
-def minimax(board, player, remain_time):
+DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1,1)]
+
+class minimax(board, player = 1, remain_time = 50):
     def is_valid_move(board, row, col, player):
         if board[row][col] != 0:
             return False
@@ -33,22 +44,57 @@ def minimax(board, player, remain_time):
                     c += j
         return False
 
-    def get_valid_moves(board, player):
+    def get_valid_moves(self,board, player):
         valid_moves = []
         for row in range(8):
             for col in range(8):
-                if is_valid_move(board, row, col, player):
+                if self.is_valid_move(board, row, col, player):
                     valid_moves.append((row, col))
         return valid_moves
 
     def Win(board, player):
-        return True if (sum(map(sum, board))*player > 0) else False
+        return True if ((np.count_nonzero(board == 0) == 0 and sum(map(sum, board))*player > 0) or np.count_nonzero(board == -1) == 0) else False
 
+    def Lose(board, player):
+        return True if ((np.count_nonzero(board == 0) == 0 and sum(map(sum, board))*player < 0) or np.count_nonzero(board == 1) == 0) else False
+    
+    def Tie(board, player):
+        return True if (np.count_nonzero(board == 0) == 0 and sum(map(sum, board))*player == 0) else False
+    
     def countDics(board, player):
         num = 0
         for row in board:
             num += row.count(player)
         return num
+    def make_move(board, row, col, player):
+            new_board = [row[:] for row in board]
+            new_board[row][col] = player
+            
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if i == 0 and j == 0:
+                        continue
+                    
+                    r = row + i
+                    c = col + j
+                    flipped = False
+                    to_flip = []
+                    
+                    while r >= 0 and r < 8 and c >= 0 and c < 8:
+                        if new_board[r][c] == 0:
+                            break
+                        if new_board[r][c] == player:
+                            flipped = True
+                            break
+                        to_flip.append((r, c))
+                        r += i
+                        c += j
+                    
+                    if flipped:
+                        for (r, c) in to_flip:
+                            new_board[r][c] = player
+            
+            return new_board
 
     def heuristic(board, player):
         h = [
@@ -138,8 +184,8 @@ def minimax(board, player, remain_time):
 
         # Mobility
         self, opp = 0, 0
-        self = len(get_valid_moves(board, player))
-        opp = len(get_valid_moves(board, player*-1))
+        self = len(self.get_valid_moves(board, player))
+        opp = len(self.get_valid_moves(board, player*-1))
         if (self > opp):
             m += (100 * self)/(self + opp)
         else:
@@ -151,8 +197,65 @@ def minimax(board, player, remain_time):
 
     a = heuristic(board, 1)
     print(a)
+    def minimax_value(self, board, white_turn, search_depth, alpha, beta):
+        if search_depth == 0:
+            return self.heuristic(board, 1)
+        if self.Tie(board, 1):
+            return 0
+        elif self.Win(board, 1):
+            return 10000
+        elif self.Lose(board, 1):
+            return -10000
+        else:
+                pass # No game condition met, continue searching down the decision tree.
+        
+        if white_turn:
+            maxScore = -sys.maxsize
+            legal_moves = self.get_valid_moves(board, 1)
 
-minimax(board, 1, 50)
+            if len(legal_moves) == 0:
+                maxScore = self.minimax_value(board, False, search_depth, alpha, beta)
+
+            else:
+                for move in legal_moves:
+                    new_board = self.make_move(board, move[0], move[1], 1) # Get the new board state
+                    score = self.minimax_value(new_board, False, search_depth - 1, alpha, beta)
+                    maxScore = max(maxScore, score)
+                    alpha = max(alpha, score)
+                    if beta <= alpha:
+                        break
+            return maxScore
+
+        else:
+            minScore = sys.maxsize
+            legal_moves = self.get_valid_moves(board, -1)
+            if len(legal_moves) == 0:
+                minScore = self.minimax_value(board, True, search_depth, alpha, beta)
+            
+            else:
+                for move in legal_moves:
+                    new_board = self.make_move(board, move[0], move[1], -1) # Get the new board state
+                    score = self.minimax_value(new_board, True, search_depth - 1, alpha, beta)
+                    minScore = min(minScore, score)
+                    beta = min(beta, score)
+                    if beta <= alpha:
+                        break
+            return minScore
+    def select_moves(self, board, player, remain_time):
+        if (player == 1):
+            best_val = float("-inf")
+            best_move = None
+            legal_moves = self.get_valid_moves(board, 1)
+            for move in legal_moves:
+                new_board = self.make_move(board, move[0], move[1], -1)
+                move_val = self.minimax_value(new_board, True, 5, float("-inf"), float("inf"))
+                if move_val > best_val:
+                    best_move = move
+                    best_val = move_val
+            return best_move
+a = minimax(board, 1, 50)
+b = a.select_moves(board, 1, 50)
+#minimax(board, 1, 50)
 
 def random_agent(cur_state, player_to_move, remain_time):
     def is_valid_move(board, row, col, player):
